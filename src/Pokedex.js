9d952932fb368995c9aebc88ec79e33a81471302
Krishan Patel <kp3099@gmail.com>
2022-08-11 10:43:22 -0400

    added layout for pokedex component

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,8 +1,49 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/styles'
+import { 
+  AppBar,
+  Toolbar,
+  Grid,
+  Card,
+  CardMedia,
+  CardContent
+} from '@material-ui/core'
+
+
+const useStyles = makeStyles({
+  pokedexContainer: {
+    paddingTop: '20px',
+    paddingLeft: '50px',
+    paddingRight: '50px',
+  }
+})
+
+const getPokemonCard = () => {
+  return(
+    <Grid item xs={12} sm={4}>
+      <Card>
+        <CardContent>hi</CardContent>
+      </Card>
+    </Grid>
+  )
+}
 
 const Pokedex = () => {
+  const classes = useStyles()
+
   return (
-    <div><h1>This is the Pokedex Page</h1></div>
+    <>
+      <AppBar position='static'>
+        <Toolbar/>
+      </AppBar>
+      <Grid container spacing={2} className={classes.pokedexContainer}>
+        {getPokemonCard()}
+        {getPokemonCard()}
+        {getPokemonCard()}
+        {getPokemonCard()}
+        {getPokemonCard()}
+      </Grid>
+    </>
   )
 }
 
