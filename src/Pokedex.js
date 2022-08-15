cfeef3af69fb2098ad1ada5d1ec245e2477e0c7e
Krishan Patel <kp3099@gmail.com>
2022-08-15 15:52:24 -0400

    replaced mockdata with calls to api

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,7 +1,9 @@
-import React, { useState } from 'react'
-import { makeStyles } from '@material-ui/styles'
-import mockData from './mockData'
+import React, { useState, useEffect } from 'react'
+import { alpha, makeStyles } from '@material-ui/core/styles'
+import SearchIcon from '@material-ui/icons/Search'
 import { toFirstCharUppercase } from './constants'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
 import { 
   AppBar,
   Toolbar,
@@ -10,11 +12,12 @@ import {
   CardMedia,
   CardContent,
   CircularProgress,
-  Typography
+  Typography,
+  TextField
 } from '@material-ui/core'
 
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   pokedexContainer: {
     paddingTop: '20px',
     paddingLeft: '50px',
@@ -26,22 +29,48 @@ const useStyles = makeStyles({
   cardContent: {
     textAlign: 'center'
   },
-})
+  searchContainer: {
+    display: 'flex',
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    paddingLeft: '20px',
+    paddingRight: '20px',
+    marginTop: '5px',
+    marginBottom: '5px',
+  }
+}))
 
 
 const Pokedex = () => {
+  const nav = useNavigate()
   const classes = useStyles()
-  const [pokemonData, setPokemonData] = useState(mockData)
+  const [pokemonData, setPokemonData] = useState(undefined)
   
+
+  useEffect(() => {
+    axios
+    .get(`https://pokeapi.co/api/v2/pokemon?limit=807`)
+    .then( (res) => {
+      const {data: {results}} = res
+      const newPokemonData = {}
+      results.forEach((pokemon,index) => {
+        newPokemonData[index+1] = {
+          id: index+1,
+          name: pokemon.name,
+          sprite: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index+1}.png`,
+        }
+      })
+      setPokemonData(newPokemonData)
+    })
+  },[])
+
+
   const getPokemonCard = (pokemonId) => {
-    // console.log(pokemonData[`${pokemonId}`])
-    const { id, name, sprites: {front_default: sprite}} = pokemonData[`${pokemonId}`]
-    // const sprite = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png"
+    const { id, name, sprite} = pokemonData[`${pokemonId}`]
 
     console.log(sprite)
     return(
       <Grid item xs={12} sm={4} key={pokemonId}>
-        <Card onClick = {() => history.push(`$/{pokemonId}`)}>
+        <Card onClick = {() => nav(`${pokemonId}`)}>
           <CardMedia 
             className={classes.cardMedia}
             image={sprite}
@@ -58,7 +87,12 @@ const Pokedex = () => {
   return (
     <>
       <AppBar position='static'>
-        <Toolbar/>
+        <Toolbar>
+          <div className={classes.searchContainer}>
+            <SearchIcon className={classes.searchIcon}/>
+            <TextField className={classes.textInput}/>
+          </div>
+        </Toolbar>
       </AppBar>
       {pokemonData ? (
         <Grid container spacing={2} className={classes.pokedexContainer}>
--- src/Pokemon.js
+++ src/Pokemon.js
@@ -1,17 +1,41 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import mockData from './mockData'
-import { Typography, Link } from '@material-ui/core'
+import { Typography, Link, CircularProgress, Button } from '@material-ui/core'
 import { toFirstCharUppercase } from './constants'
-
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
 
 const Pokemon = () => {
+  const nav = useNavigate()
   const { pokemonId } = useParams()
   const [pokemon, setPokemon] = useState(mockData[`${pokemonId}`])
 
+  useEffect(() => {
+    axios
+    .get(`https://pokeapi.co/api/v2/pokemon/${pokemonId}/`)
+    .then((res) => {
+      const {data} = res
+      setPokemon(data)
+    })
+    .catch((err) => {
+      setPokemon(false)
+    })
+  }, [pokemonId])
+
+
+  //states
+  //1. pokemon = undefined, getting info
+  // return -> loading progress
+  //2. pokemon = good data, got info
+  // return -> actual info
+  //3. pokemon = bad data / false
+  // return -> pokemon not found
+
   const generatePokemonJSX = () => {
     const { name, id, species, height, weight, types, sprites } = pokemon
-    const fullImageUrl = `https://pokeres.bastionbot.org/images/pokemon/${id}.png`
+    // const fullImageUrl = `https://pokeres.bastionbot.org/images/pokemon/${id}.png`
+    const fullImageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`
     const {front_default} = sprites
     return (
       <>
@@ -25,12 +49,28 @@ const Pokemon = () => {
           {'Species: '}
           <Link href={species.url}>{species.name}</Link>
         </Typography>
+        <Typography>Height: {height} </Typography>
+        <Typography>Weight: {weight} </Typography>
+        <Typography variant='h6'>Types:</Typography>
+        {types.map((typeInfo) => {
+          const { type: {name} } = typeInfo;
+          return <Typography key={name}> {`${name}`} </Typography>
+        })}
       </>
     )
   }
 
   return (
-    <>{generatePokemonJSX()}</>
+    <>
+      {pokemon === undefined && <CircularProgress/>}
+      {pokemon !== undefined && pokemon && generatePokemonJSX()}
+      {pokemon === false && <Typography>Pokemon not found</Typography>}
+      {pokemon !== undefined && (
+        <Button variant="contained" onClick={() => nav('/')}>
+          Back to Pokedex
+        </Button>
+      )}
+    </>
   )
 }
 
